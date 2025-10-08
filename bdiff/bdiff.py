02989110c5db9d88dc74f9501a5d77b14f61efc9
Zhikang Yan <2951256653@qq.com>
2025-10-08 19:49:45 +0800

    style(src): fix the style of copyright in source code

--- bdiff/bdiff.py
+++ bdiff/bdiff.py
@@ -1,14 +1,14 @@
-"""
-Copyright (c) [2025] [Lu YAO]
-BDiff is licensed under Mulan PubL v2.
-You can use this software according to the terms and conditions of the Mulan PubL v2.
-You may obtain a copy of Mulan PubL v2 at:
-        http://openworks.mulanos.cn/#/licenses/MulanPubL-v2
-THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-See the Mulan PubL v2 for more details.
-"""
+# Copyright (c) [2025] [Lu YAO]
+# BDiff is licensed under Mulan PubL v2.
+# You can use this software according to the terms and conditions of the Mulan PubL v2.
+# You may obtain a copy of Mulan PubL v2 at:
+#         http://openworks.mulanos.cn/#/licenses/MulanPubL-v2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PubL v2 for more details.
+
+"""Core codes of BDiff."""
 
 import copy
 import os
