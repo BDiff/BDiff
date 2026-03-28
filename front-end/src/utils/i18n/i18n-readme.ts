2ddf42d554ae3aea6a6ccf5a56b4df192fd79eda
宋万盛 <951379203@qq.com>
2026-03-28 10:47:16 +0800

    更新许可证

--- front-end/src/utils/i18n/i18n-readme.ts
+++ front-end/src/utils/i18n/i18n-readme.ts
@@ -74,7 +74,7 @@ export const I18N_MESSAGES_README = {
         `<p>This software is licensed under <a href="http://openworks.org.cn/#/licenses/MulanPubL-v2">Mulan Public License，Version 2</a> (Mulan PubL v2).</p>` +
         `<div class="bg-light">` +
         `<pre class="p-3">` +
-        `<code>Copyright (c) [2025] [Lu YAO]
+        `<code>Copyright (c) [2025] [**]
 BDiff is licensed under Mulan PubL v2.
 You can use this software according to the terms and conditions of the Mulan PubL v2.
 You may obtain a copy of Mulan PubL v2 at:
@@ -216,7 +216,7 @@ See the Mulan PubL v2 for more details.</code>` +
         `<p>BDiff使用<a href="http://openworks.mulanos.cn/#/licenses/MulanPubL-v2" target="_blank">木兰公共许可证 v2</a>开源：</p>` +
         `<div class="bg-light">` +
         `<pre class="p-3">` +
-        `<code>Copyright (c) [2025] [Lu YAO]
+        `<code>Copyright (c) [2025] [**]
 BDiff is licensed under Mulan PubL v2.
 You can use this software according to the terms and conditions of the Mulan PubL v2.
 You may obtain a copy of Mulan PubL v2 at:
